08e4a0a5d4ae68b581ad4b1a64167c05f779cd44
unknown <l.gamm960817@itses.edu.mx>
2019-11-06 19:00:10 -0600

    api rest

--- petsfriend/src/app/app.module.ts
+++ petsfriend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-
+import {HttpClientModule} from "@angular/common/http";
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
@@ -9,6 +9,7 @@ import { LoginPage} from "../pages/login/login";
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { RestProvider } from '../providers/rest/rest';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -31,7 +33,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    RestProvider
   ]
 })
 export class AppModule {}
---
+++ petsfriend/src/providers/rest/rest.ts
@@ -0,0 +1,31 @@
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the RestProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class RestProvider {
+
+  apiUrl = 'http://petsfriend.herokuapp.com';
+
+
+
+  constructor(public http: HttpClient) {
+    console.log('Hello RestProvider Provider');
+  }
+  getUsers() {
+    return new Promise(resolve => {
+      this.http.get(this.apiUrl+'/especie').subscribe(data => {
+        resolve(data);
+        console.log(data);
+      })
+
+
+    });
+  }
+
+}
